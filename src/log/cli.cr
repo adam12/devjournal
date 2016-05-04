277229fdea4a848a1ca253add507bca5ddfa45dd
Adam Daniels <adam@mediadrive.ca>
2016-05-04 00:56:54 -0400

    Refactor CLI into class with small methods

--- src/log/cli.cr
+++ src/log/cli.cr
@@ -2,64 +2,94 @@ require "option_parser"
 require "colorize"
 
 module Log
-  module CLI
-    def self.run
-      type = nil
-      project = nil
-      search_text = nil
-      clear_last_entry = false
+  class CLI
+    def initialize(@argv)
+      parse_options
+
+      @logfile = Logfile.new("foo.sqlite")
+    end
+
+    def run
+      case
+      when @search_text then search
+      when @clear_last_entry then clear_last_entry
+      when @argv[0]? && @argv[0] == "-" then stdin_input
+      when @argv[0]? then argv_input
+      else show_recent_entries
+      end
+    end
+
+    def search
+      entries = @logfile.search_entries(@search_text)
+      display_entries(entries)
+    end
+
+    def show_recent_entries
+      entries = @logfile.entries
+      display_entries(entries)
+    end
+
+    def display_entries(entries)
+      entries.each do |entry|
+        body = entry[0]
+        type = entry[1]
+        project = entry[2]
+        created_at = entry[3]
+
+        puts created_at
+        puts "Type: #{type}".colorize(:blue) if type
+        puts "Project: #{project}".colorize(:yellow) if project
+        puts
+        puts body
+        puts
+      end
+    end
+
+    def stdin_input
+      lines = [] of String
+
+      while (line = STDIN.gets)
+        lines << line
+      end
+
+      body = lines.join("\n")
+      add_entry(body)
+    end
+
+    def argv_input
+      body = ARGV[0..-1].join("\n")
+      add_entry(body)
+    end
+
+    def add_entry(body)
+      @logfile.add_entry(body, @type, @project)
+    end
+
+    def clear_last_entry
+      @logfile.clear_last_entry
+    end
+
+    def parse_options
+      @type = nil
+      @project = nil
+      @search_text = nil
+      @clear_last_entry = false
 
       OptionParser.parse! do |parser|
         parser.banner = "Usage: #{$0} [arguments]"
-        parser.on("-t TYPE", "--type=TYPE", "The type classification that the log event belongs to. example: work, school etc.") { |t| type = t }
+        parser.on("-t TYPE", "--type=TYPE", "The type classification that the log event belongs to. example: work, school etc.") { |t| @type = t }
         parser.on("-p PROJECT", "--project=PROJECT", "The project that the log event belongs to. This helps group log events together
-                                     which might belong to the same type or which my not belong to a type at all.") { |p| project = p }
+                                     which might belong to the same type or which my not belong to a type at all.") { |p| @project = p }
 
-        parser.on("-s TEXT", "Case insensitive search of the log file for the given text.") { |s| search_text = s }
-        parser.on("-x", "Deletes the last entry from the logfile.") { clear_last_entry = true }
+        parser.on("-s TEXT", "Case insensitive search of the log file for the given text.") { |s| @search_text = s }
+        parser.on("-x", "Deletes the last entry from the logfile.") { @clear_last_entry = true }
         parser.on("-h", "--help", "Show this help") { abort(parser) }
+        parser.on("-v", "--version", "Show the version") { abort("Log v#{Log::VERSION}") }
       end
+    end
 
-      logfile = Logfile.new("foo.sqlite")
-
-      case
-      when search_text
-        puts "Search text: #{search_text}"
-        logfile.search_entries(search_text).each do |entry|
-          body = entry[0]
-          type = entry[1]
-          project = entry[2]
-          created_at = entry[3]
-
-          puts "#{created_at} - #{body}"
-        end
-      when clear_last_entry
-        logfile.clear_last_entry
-      when ARGV[0]? && ARGV[0] == "-"
-        lines = [] of String
-        while (line = STDIN.gets)
-          lines << line
-        end
-        body = lines.join("\n")
-        logfile.add_entry(body, type, project)
-      when ARGV[0]?
-        body = ARGV[0..-1].join(" ")
-        logfile.add_entry(body, type, project)
-      else
-        logfile.entries.each do |entry|
-          body = entry[0]
-          type = entry[1]
-          project = entry[2]
-          created_at = entry[3]
-
-          puts created_at
-          puts "Type: #{type}".colorize(:blue) if type
-          puts "Project: #{project}".colorize(:yellow) if project
-          puts
-          puts body
-          puts
-        end
-      end
+    def self.run
+      self.new(ARGV).run
     end
   end
 end
